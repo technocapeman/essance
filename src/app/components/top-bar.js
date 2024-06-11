584b5176d8ae060e68ed40940a5558404b23ecb2
Kapilesh Pennichetty <kapilesh.pennichetty@gmail.com>
2024-06-10 23:02:37 -0500

    Link back to home page

--- src/app/components/top-bar.js
+++ src/app/components/top-bar.js
@@ -4,6 +4,7 @@ export const TopBar = () => {
     return (
         <div className="flex flex-col pt-5 w-full max-w-screen-xl mx-auto">
             <div className="flex flex-wrap items-center justify-between gap-5 w-full px-5">
+                <a href="/">
                 <div className="flex items-center gap-5">
                     <div className="flex flex-col justify-center items-center px-4 rounded-full bg-orange-200 bg-opacity-40 h-[50px] w-[50px] sm:h-[70px] sm:w-[70px] md:h-[89px] md:w-[89px]">
                         <div className="shrink-0 rounded-full bg-orange-200 bg-opacity-40 h-[35px] w-[35px] sm:h-[48px] sm:w-[48px] md:h-[61px] md:w-[61px]" />
@@ -12,6 +13,7 @@ export const TopBar = () => {
                         Essence
                     </div>
                 </div>
+                </a>
                 <div className="flex items-center gap-5 mt-5 sm:mt-0">
                     <div className="text-lg sm:text-2xl md:text-3xl text-black">Pricing</div>
                     <button className="shrink-0 border-gray-200 border-solid border-[3px] h-[40px] sm:h-[60px] md:h-[80px] lg:h-[105px] rounded-[25px] sm:rounded-[35px] md:rounded-[50px] lg:rounded-[80px] w-[120px] sm:w-[180px] md:w-[240px] lg:w-[336px]">
